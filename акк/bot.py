8014746b88f966f2cb90f656744ef64499ba340a
FloxHant <buandaniil10@gmail.com>
2025-10-05 12:45:17 +0300

    Update bot.py

--- акк/bot.py
+++ акк/bot.py
@@ -1,27 +1,26 @@
-from config import token
-import telebot
-from random import choice
+import telebot # библиотека telebot
+from config import token # импорт токена
 
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(token) 
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
+@bot.message_handler(commands=['ban'])
+def ban_user(message):
+    if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
+        chat_id = message.chat.id # сохранение id чата
+         # сохранение id и статуса пользователя, отправившего сообщение
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status 
+         # проверка пользователя
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно забанить администратора.")
+        else:
+            bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
+            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
+    else:
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
-
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
-@bot.message_handler(commands=['coin'])
-def coin_handler(message):
-    coin = choice(["ОРЕЛ", "РЕШКА"])
-    bot.reply_to(message, coin)
-
-bot.infinity_polling()
+bot.infinity_polling(none_stop=True)
