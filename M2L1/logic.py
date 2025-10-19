06245bbb1199735ef265a6280209cbaa41dff003
FloxHant <buandaniil10@gmail.com>
2025-10-19 19:26:05 +0300

    Update logic.py

--- M2L1/logic.py
+++ M2L1/logic.py
@@ -1,5 +1,6 @@
 from random import randint
-
+import time
+from datetime import datetime
 import requests
 
 
@@ -16,8 +17,19 @@ class Pokemon:
 
         self.hp = randint(20, 40)
         self.power = randint(5, 15)
-
+        self.feed = randint(5, 30)
+        self.last_feed_time = datetime.datetime.now()
         Pokemon.pokemons[pokemon_trainer] = self
+
+    def feed(self, feed_interval = 20, hp_increase = 10 ):
+        current_time = datetime.now()  
+        delta_time = datetime.timedelta(seconds=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}"
+        else:
+            return f"Следующее время кормления покемона: {self.last_feed_time+delta_time}"
 
     def attack(self, enemy):
         if enemy.hp > self.power:
@@ -54,6 +66,7 @@ class Pokemon:
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
+
 
 
 
